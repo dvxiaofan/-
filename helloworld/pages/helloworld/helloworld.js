8c1295d9ee4064d377c54a04130b4735177b2daa
xiaofan <1353815633@qq.com>
2018-03-27 23:16:14 +0800

    test

--- helloworld/pages/helloworld/helloworld.js
+++ helloworld/pages/helloworld/helloworld.js
@@ -1,1 +1,14 @@
-Page({})
+Page({
+  getProfile(res){
+    // console.log(res.detail.userInfo);
+    this.setData({
+      'profile':res.detail.userInfo
+    })
+  },
+  data:{
+    'profile':{
+      nickName:'小明同学',
+      'avatarUrl':'../../img/icon.jpg'
+    }
+  }
+})
