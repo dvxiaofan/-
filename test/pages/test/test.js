3446ce72fa5147d5fab0fa8b4f48cb67edb00e07
xiaofan <1353815633@qq.com>
2018-04-02 11:33:48 +0800

    setData

--- test/pages/test/test.js
+++ test/pages/test/test.js
@@ -1,1 +1,36 @@
-Page({})
+Page({
+  data:{
+    text:'init data',
+    num: 0,
+    array:[{text:'init data'}],
+    object:{
+      text:'init data'
+    }
+  },
+  changeText:function(){
+    this.setData({
+      text:"change text"
+    })
+  },
+  changeNum:function(){
+    this.data.num = 9
+    this.setData({
+      num: this.data.num
+    })
+  },
+  changeArrayData:function(){
+    this.setData({
+      'array[0].text' : 'changed text'
+    })
+  },
+  changeObjectData:function(){
+    this.setData({
+      'object.text' : 'change data'
+    })
+  },
+  addNewField: function () {
+    this.setData({
+      'newField.text': 'new data'
+    })
+  }
+})
