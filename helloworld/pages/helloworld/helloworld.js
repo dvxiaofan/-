d28cf1dd3f58d5d4543636cdf987c8d64c8f3408
xiaofan <1353815633@qq.com>
2018-04-02 09:35:20 +0800

    0402

--- helloworld/pages/helloworld/helloworld.js
+++ helloworld/pages/helloworld/helloworld.js
@@ -1,14 +1,17 @@
 Page({
-  getProfile(res){
-    // console.log(res.detail.userInfo);
-    this.setData({
-      'profile':res.detail.userInfo
-    })
-  },
+  // getProfile(res){
+  //   // console.log(res.detail.userInfo);
+  //   this.setData({
+  //     'profile':res.detail.userInfo
+  //   })
+  // },
+  // data:{
+  //   'profile':{
+  //     nickName:'小明同学',
+  //     'avatarUrl':'../../img/icon.jpg'
+  //   }
+  // }
   data:{
-    'profile':{
-      nickName:'小明同学',
-      'avatarUrl':'../../img/icon.jpg'
-    }
+    flag: false
   }
 })
