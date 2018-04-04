494d3c397d626ea7b1e2a1f43333781dcac91529
xiaofan <1353815633@qq.com>
2018-04-04 10:34:19 +0800

    条件选软

--- test/pages/test/test.js
+++ test/pages/test/test.js
@@ -49,6 +49,57 @@ Page({
 //   })
 // }
 
+data:{
+  objectArray:[
+    { id: 5, unique: 'unique_5' },
+    { id: 4, unique: 'unique_4' },
+    { id: 3, unique: 'unique_3' },
+    { id: 2, unique: 'unique_2' },
+    { id: 1, unique: 'unique_1' },
+    { id: 0, unique: 'unique_0' },
+  ],
+  numArray:[1,2,3,4]
+},
+switch:function(e){
+  const length = this.data.objectArray.length
+  for (let i = 0; i < length; ++i){
+    const x = Math.floor(Math.random() * length)
+    const y = Math.floor(Math.random() * length)
+    const temp = this.data.objectArray[x]
+    this.data.objectArray[x] = this.data.objectArray[y]
+    this.data.objectArray[y] = temp
+  }
+  this.setData({
+    objectArray:this.data.objectArray
+  })
+},
+addToFrond:function(e){
+  const length = this.data.objectArray.length
+  this.data.objectArray = [{id: length, unique: 'unique'+length}].concat(this.data.objectArray)
+  this.setData({
+    objectArray : this.data.objectArray
+  })
+},
+  addNumToFrond:function(e){
+    this.data.numArray = [this.data.numArray.legth + 1].concat(this.data.numArray)
+    this.setData({
+      numArray: this.data.numArray
+    })
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 })
