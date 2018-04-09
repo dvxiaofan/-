99ba05fd6a7f5f40014837894a1075fea85a652f
xiaofan <1353815633@qq.com>
2018-04-09 14:49:37 +0800

    cover view

--- 组件/pages/index/index.js
+++ 组件/pages/index/index.js
@@ -1,18 +1,31 @@
+// Page({
+//   data: {
+//     x: 0,
+//     y: 0
+//   },
+//   tap: function (e) {
+//     this.setData({
+//       x: 30,
+//       y: 30
+//     });
+//   },
+//   onChange: function (e) {
+//     console.log(e.detail)
+//   },
+//   onScale: function (e) {
+//     console.log(e.detail)
+//   }
+// })
+
+
 Page({
-  data: {
-    x: 0,
-    y: 0
+  onReady() {
+    this.videoCtx = wx.createVideoContext('myVideo')
   },
-  tap: function (e) {
-    this.setData({
-      x: 30,
-      y: 30
-    });
+  play() {
+    this.videoCtx.play()
   },
-  onChange: function (e) {
-    console.log(e.detail)
-  },
-  onScale: function (e) {
-    console.log(e.detail)
+  pause() {
+    this.videoCtx.pause()
   }
 })
