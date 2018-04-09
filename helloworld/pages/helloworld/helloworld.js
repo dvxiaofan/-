b8d2fa9c1199b0d463ef071280fc2d82e2aef428
xiaofan <1353815633@qq.com>
2018-04-09 14:41:06 +0800

    scroll view

--- helloworld/pages/helloworld/helloworld.js
+++ helloworld/pages/helloworld/helloworld.js
@@ -1,4 +1,4 @@
-Page({
+// Page({
   // getProfile(res){
   //   // console.log(res.detail.userInfo);
   //   this.setData({
@@ -58,4 +58,111 @@ Page({
 
 
 
+// })
+
+var order = ['red', 'yellow', 'blue', 'green'];
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    toView: 'red',
+    scrollTop: 0
+  },
+
+  scroll: function(e) {
+    console.log(e)
+  },
+  upper: function(e) {
+    console.log(e)
+  },
+  lower: function(e) {
+    console.log(e)
+  },
+  tap: function(e) {
+    for (var i = 0; i < order.length; ++i) {
+      if(order[i] === this.data.toView) {
+        this.setData({
+          toView: order[i + 1]
+        })
+        break
+      }
+    }
+  },
+
+
+
+
+
+
+
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+    
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    
+  }
 })
+
+
+
+
+
+
+
+
+
+
+
+
