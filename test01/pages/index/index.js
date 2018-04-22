33f478aaa2c0c4a8695d687df5c22ac2610d5bb5
xiaofan <1353815633@qq.com>
2018-04-22 20:24:49 +0800

    rich-text

--- test01/pages/index/index.js
+++ test01/pages/index/index.js
@@ -2,25 +2,63 @@
 //获取应用实例
 // const app = getApp()
 
-var initData = "This is the first line \nThis is the second line";
-var extraLine = [];
+// var initData = "This is the first line \nThis is the second line";
+// var extraLine = [];
 
 Page({
+  // data: {
+  //   text: initData
+  // },
+  // add: function(e) {
+  //   extraLine.push("Other line")
+  //   this.setData({
+  //     text: initData + '\n' + extraLine.join('\n')
+  //   })
+  // },
+  // remove: function(e) {
+  //   if(extraLine.length > 0) {
+  //     extraLine.pop()
+  //     this.setData({
+  //       text: initData + '\n' + extraLine.join('\n')
+  //     })
+  //   }
+  // }
+
+// 富文本
   data: {
-    text: initData
+    nodes: [{
+      name: 'div',
+      attrs: {
+        class: 'div_class',
+        style: 'line-height: 60px; color: red;'
+      },
+      children: [{
+        type: 'text',
+        text: 'Hello&nbsp;World!'
+      }]
+    }]
   },
-  add: function(e) {
-    extraLine.push("Other line")
-    this.setData({
-      text: initData + '\n' + extraLine.join('\n')
-    })
-  },
-  remove: function(e) {
-    if(extraLine.length > 0) {
-      extraLine.pop()
-      this.setData({
-        text: initData + '\n' + extraLine.join('\n')
-      })
-    }
+  tap() {
+    console.log('tap');
   }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 })
