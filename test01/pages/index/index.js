9598b59cf22007171de50953792bc3bfd1eb1a52
xiaofan <1353815633@qq.com>
2018-05-13 01:17:32 +0800

    navigator

--- test01/pages/index/index.js
+++ test01/pages/index/index.js
@@ -25,22 +25,22 @@ Page({
   // }
 
 // 富文本
-  data: {
-    nodes: [{
-      name: 'div',
-      attrs: {
-        class: 'div_class',
-        style: 'line-height: 60px; color: red;'
-      },
-      children: [{
-        type: 'text',
-        text: 'Hello&nbsp;World!'
-      }]
-    }]
-  },
-  tap() {
-    console.log('tap');
-  }
+  // data: {
+  //   nodes: [{
+  //     name: 'div',
+  //     attrs: {
+  //       class: 'div_class',
+  //       style: 'line-height: 60px; color: red;'
+  //     },
+  //     children: [{
+  //       type: 'text',
+  //       text: 'Hello&nbsp;World!'
+  //     }]
+  //   }]
+  // },
+  // tap() {
+  //   console.log('tap');
+  // }
 
 
 
